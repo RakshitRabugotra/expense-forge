56ca09bb7c9f092124afa4355aa718d4790e97ac
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2024-06-10 07:01:12 +0530

    add: new styles to dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -9,15 +9,15 @@ import {
   currencyFormatterINR,
 } from '@/utils/functions/currency'
 
-// Internal Dependencies
-import SubHeading from './sub-heading'
-
 // Custom Actions
 import { getUserPersonalizations } from '@/actions/user-personalization'
 import { getExpenseThisMonth, getExpensesToday } from '@/actions/stats'
 import { reduceExpenses } from '@/utils/functions/expenses'
 
 export default function Dashboard() {
+  // The state for selecting the selected dash
+  const [isDaily, setIsDaily] = useState<boolean>(true)
+
   // The state variables for the dashboard
   const [monthlyGoal, setMonthlyGoal] = useState<number>(0)
   const [thisMonth, setThisMonth] = useState<number>(0)
@@ -47,31 +47,74 @@ export default function Dashboard() {
   console.log({ monthlyGoal })
 
   return (
-    <section className='flex w-full flex-col items-center'>
-      <SubHeading>Daily Tracker</SubHeading>
-      <fieldset className='flex w-full flex-row rounded-xl border bg-foreground [&>*]:basis-1/2'>
-        <ExpenseField title="today's expense" figure={thisDay} />
-        <ExpenseField title='daily goal' figure={dailyGoal} />
-      </fieldset>
+    <section className='flex w-full flex-col items-center rounded-xl border bg-black/50 p-3'>
+      <div className='flex w-full flex-row items-center justify-between'>
+        <button
+          onClick={() => setIsDaily(true)}
+          className={twMerge(
+            isDaily
+              ? 'border-b-emerald-500 font-bold text-white/80'
+              : 'border-b-black/10 font-normal text-black/50',
+            'm-2 mx-4 border-b-4 px-2',
+            'transition-all duration-200 ease-in-out',
+          )}
+        >
+          Daily Tracker
+        </button>
+        <button
+          onClick={() => setIsDaily(false)}
+          className={twMerge(
+            !isDaily
+              ? 'border-b-emerald-500 font-bold text-white/80'
+              : 'border-b-black/10 font-normal text-black/50',
+            'm-2 mx-4 border-b-4 px-2',
+            'transition-all duration-200 ease-in-out',
+          )}
+        >
+          Monthly Tracker
+        </button>
+      </div>
 
-      <SubHeading>Monthly Tracker</SubHeading>
-      <fieldset className='flex w-full flex-row rounded-xl border bg-foreground [&>*]:basis-1/2'>
-        <ExpenseField title='expenditure' figure={thisMonth} />
-        <ExpenseField title='monthly goal' figure={monthlyGoal} />
-      </fieldset>
+      <Dash
+        current={
+          isDaily
+            ? { title: 'expense today', figure: thisDay }
+            : { title: 'monthly expense', figure: thisMonth }
+        }
+        goal={
+          isDaily
+            ? { title: 'daily goal', figure: dailyGoal }
+            : { title: 'monthly goal', figure: monthlyGoal }
+        }
+      />
     </section>
   )
 }
 
-function ExpenseField({
-  title,
-  figure,
-  onClick,
+function Dash({
+  current,
+  goal,
 }: {
+  current: ExpenseFieldProps
+  goal: ExpenseFieldProps
+}) {
+  return (
+    <>
+      <fieldset className='flex w-full flex-row rounded-xl bg-foreground [&>*]:basis-1/2'>
+        <ExpenseField {...current} />
+        <ExpenseField {...goal} />
+      </fieldset>
+    </>
+  )
+}
+
+type ExpenseFieldProps = {
   title: string
   figure: number
   onClick?: React.MouseEventHandler<HTMLDivElement>
-}) {
+}
+
+function ExpenseField({ title, figure, onClick }: ExpenseFieldProps) {
   // Use a count-up animation
   const { number } = useSpring({
     from: { number: 0 },
@@ -83,13 +126,14 @@ function ExpenseField({
   return (
     <div
       className={twMerge(
+        'flex flex-col',
         'select-none text-white',
         'p-2 px-4',
         onClick ? 'cursor-pointer' : '',
       )}
       onClick={onClick}
     >
-      <p className='break-words text-base font-medium capitalize text-white/70'>
+      <p className='grow break-words text-base font-medium capitalize text-white/70'>
         {title}
       </p>
       <animated.h2 className='max-w-full break-words text-3xl font-bold leading-snug'>
